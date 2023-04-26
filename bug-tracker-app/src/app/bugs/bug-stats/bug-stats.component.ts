8ffa721a6bebf73995cba1ee2e423ef642c9e79d
Magesh Kuppan <tkmagesh77@gmail.com>
2023-04-27 01:58:08 +0530

    Using ChangeDetectionStrategy.OnPush

--- bug-tracker-app/src/app/bugs/bug-stats/bug-stats.component.ts
+++ bug-tracker-app/src/app/bugs/bug-stats/bug-stats.component.ts
@@ -1,13 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Bug } from '../models/bug';
 
 @Component({
   selector: 'app-bug-stats',
   templateUrl: './bug-stats.component.html',
-  styleUrls: ['./bug-stats.component.css']
+  styleUrls: ['./bug-stats.component.css'],
+  changeDetection : ChangeDetectionStrategy.OnPush
 })
 export class BugStatsComponent {
 
+
   @Input('data')
   bugs : Bug[] = []
+
+  getCurrrentTime(): string {
+    return Date()
+  }
 }
