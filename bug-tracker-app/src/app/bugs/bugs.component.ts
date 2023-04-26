a23f6683c3daf71833e6b765c1bde5effa3c88b7
Magesh Kuppan <tkmagesh77@gmail.com>
2023-04-27 02:44:07 +0530

    Day-02 complete

--- bug-tracker-app/src/app/bugs/bugs.component.ts
+++ bug-tracker-app/src/app/bugs/bugs.component.ts
@@ -21,10 +21,10 @@ export class BugsComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    this.bugs.push({id : 1, name : 'Server communication failure', isClosed : false, createdAt : new Date('10-Mar-2023')})
+    /* this.bugs.push({id : 1, name : 'Server communication failure', isClosed : false, createdAt : new Date('10-Mar-2023')})
     this.bugs.push({ id: 2, name: 'Application not responding', isClosed: true, createdAt: new Date('10-Feb-2023') })
     this.bugs.push({ id: 3, name: 'User actions not recognized', isClosed: false, createdAt: new Date('10-Jan-2023') })
-    this.bugs.push({ id: 4, name: 'Data integrity checks failed', isClosed: true, createdAt: new Date('10-Apr-2023') })
+    this.bugs.push({ id: 4, name: 'Data integrity checks failed', isClosed: true, createdAt: new Date('10-Apr-2023') }) */
   }
 
   //event handler function for the bugCreate event of the BugEdit component
